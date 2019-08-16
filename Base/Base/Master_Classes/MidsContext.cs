c4e8bcdebc7412c93e1474ab0bb048cf8c9a2584
Crytilis <crytilis@gmail.com>
2019-08-16 01:03:31 -0500

    updated assembly version and fixed typo in exception

--- Base/Base/Master_Classes/MidsContext.cs
+++ Base/Base/Master_Classes/MidsContext.cs
@@ -11,18 +11,20 @@ namespace Base.Master_Classes
         public const int AppMajorVersion = 2;
         public const int AppMinorVersion = 6;
         public const int AppBuildVersion = 0;
-        public const int AppRevisionVersion = 2;
-        public const string AppAssemblyVersion = "2.6.0.2";
+        public const int AppRevisionVersion = 4;
+        public const string AppAssemblyVersion = "2.6.0.4";
         public static readonly Version AppVersion = new Version(major: AppMajorVersion, minor: AppMinorVersion, build: AppBuildVersion, revision: AppRevisionVersion);
         public const string Title = "Mids' Reborn : Hero Designer";
         public const string AssemblyName = "Hero Designer.exe";
+/*
         public const string DownloadUrl = "https://github.com/ImaginaryDevelopment/imaginary-hero-designer/releases";
+*/
         public static Archetype Archetype;
         public static Character Character;
         public static void AssertVersioning()
         {
             if (AppAssemblyVersion != $"{AppMajorVersion}.{AppMinorVersion}.{AppBuildVersion}.{AppRevisionVersion}")
-                throw new InvalidOperationException("Program assemly versioning is not internally consistent");
+                throw new InvalidOperationException("Program assembly versioning is not internally consistent");
             if (AppVersion.CompareTo(new Version(AppAssemblyVersion)) != 0)
                 throw new InvalidOperationException("Program app version is not internally consistent, failing startup");
         }
